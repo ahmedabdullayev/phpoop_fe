faac975f83ea90dff2af96055b81415e2d0ccc58
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-09-27 18:44:44 +0300

    test without hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Index from '../views/Index.vue'
 import AddProduct from '../views/AddProduct.vue'
 const routes: Array<RouteRecordRaw> = [
@@ -16,7 +16,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
