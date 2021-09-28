c293382079136cbfb84c8527b89f53b2d2e2826c
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-09-28 13:15:02 +0300

    test tester

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,22 @@
 import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Index from '../views/Index.vue'
 import AddProduct from '../views/AddProduct.vue'
+import testForm from '../views/testForm.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Index',
     component: Index
   },
+  // {
+  //   path: '/add-product',
+  //   name: 'AddProduct',
+  //   component: AddProduct
+  // },
   {
     path: '/add-product',
-    name: 'AddProduct',
-    component: AddProduct
+    name: 'testForm',
+    component: testForm
   }
 
 ]
---
+++ src/views/testForm.vue
@@ -0,0 +1,17 @@
+<template>
+<form id="#product_form">
+  <input type="text">
+</form>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "testForm"
+})
+</script>
+
+<style scoped>
+
+</style>
